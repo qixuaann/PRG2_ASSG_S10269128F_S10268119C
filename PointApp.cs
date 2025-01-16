78fc3773d065d0c8e4dcdc698147a8136542f31a
Kang Qi Xuan <s10269128@connect.np.edu.sg>
2025-01-16 14:22:36 +0800

    Delete PointApp.cs

--- PointApp.cs
+++
@@ -1,9 +0,0 @@
-﻿using PointApp;
-
-//create 2 rect objects
-Point ptA = new Point(2, 2);
-Point ptB = new Point(5, 5);
-
-// Distance between 2 points
-Console.WriteLine($"Distance between A and B: {ptA.Distance(ptB):0.00}");
-Console.WriteLine($"Distance between B and A: {ptB.Distance(ptA):0.00}");
