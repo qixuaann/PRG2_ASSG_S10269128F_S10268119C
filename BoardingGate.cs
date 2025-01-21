da1fc6286e1ba7614468d47270c4afda266af944
jjoyce003 <joycebei2007@gmail.com>
2025-01-21 21:20:35 +0800

    Implemented BoardingGate class

---
+++ BoardingGate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class BoardingGate
+{
+    public string GateName { get; set; }
+    public bool SupportsCFFT { get; set; }
+    public bool SupportsDDJB { get; set; }
+    public bool SupportsLWTT{ get; set; }
+    public Flight Flight { get; set; }
+
+
+    public BoardingGate(string gn, bool supportsCFFT, bool supportsDDJB, bool supportsLWTT)
+	{
+        GateName = gn;
+        SupportsCFFT = supportsCFFT;
+        SupportsDDJB = supportsDDJB;
+        SupportsLWTT = supportsLWTT;
+        // since there is not flight assigned yet
+        Flight = null; 
+    }
+
+    public double CalculateFees()
+    {
+        double baseFee = 300; 
+        if (Flight is CFFTFlight) baseFee += 150;
+        if (Flight is DDJBFlight) baseFee += 300;
+        if (Flight is LWTTFlight) baseFee += 500;
+        return baseFee;
+    }
+
+    public override string ToString()
+    {
+        string flightNumber = "None";
+        if (Flight != null)
+        {
+            flightNumber = Flight.FlightNumber;
+        }
+        return $"Gate: {GateName}\t Flight: {flightNumber}";
+    }
+}
